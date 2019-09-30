a3de9fa03612f531351cb214c9a09aa097e7f31b
Hasan Masud <36947488+baymax1587@users.noreply.github.com>
2019-09-30 12:56:46 +0600

    Add files via upload

--- js/info.js
+++ js/info.js
@@ -29,10 +29,12 @@ function createTable() {
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
         var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
         cell1.innerHTML = alldata[index].name;
         cell2.innerHTML = alldata[index].company;
         cell3.innerHTML = alldata[index].email;
-		cell4.innerHTML = alldata[index].message;
+        cell4.innerHTML = alldata[index].phone;
+		    cell5.innerHTML = alldata[index].message;
     }
   }, errEmailsData);
 }
